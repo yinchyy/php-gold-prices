ff7307ec8edc625b10985fa4f4e7952d8a836d15
Szymon Studniarz <szymek1019@gmail.com>
2022-07-13 18:36:18 +0200

    fetch data from API based on JSON payload

--- src/Controller/GoldController.php
+++ src/Controller/GoldController.php
@@ -4,17 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GoldController extends AbstractController
 {
     #[Route('/api/gold', name: 'app_gold', methods: [ 'POST' ])]
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+        $timestamps = json_decode($request->getContent(),true);
+        $startDate = date('Y-m-d',strtotime($timestamps["from"]));
+        $endDate = date('Y-m-d',strtotime($timestamps["to"]));
+        $url = "http://api.nbp.pl/api/cenyzlota/{$startDate}/{$endDate}";
         return $this->json([
             'from' => '2001-01-04T00:00:00+00:00',
             'to' => '2001-01-04T00:00:00+00:00',
-            'avg' => 228.1
+            'avg' => 228.1,
+            'response' => json_decode(file_get_contents($url))
         ]);
     }
 }
