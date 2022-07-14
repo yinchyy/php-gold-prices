f70eb6185ff46ee9f322e45730006e70d422d2ce
Szymon Studniarz <szymek1019@gmail.com>
2022-07-14 14:24:57 +0200

    fix tests

--- tests/GoldControllerTest.php
+++ tests/GoldControllerTest.php
@@ -11,17 +11,17 @@ class GoldControllerTest extends WebTestCase
     public function testGoldJanuary2021Single()
     {
         $client = static::createClient();
-        $client->xmlHttpRequest('POST', '/api/gold', [
-            'from' => '2001-01-04T00:00:00+00:00',
-            'to' => '2001-01-04T00:00:00+00:00'
+        $client->jsonRequest('POST', '/api/gold', [
+            'from' => '2021-01-04T00:00:00+00:00',
+            'to' => '2021-01-04T00:00:00+00:00'
         ]);
         $response = json_decode($client->getResponse()->getContent(), true);
         $this->assertResponseIsSuccessful();
         $this->assertIsArray($response);
         $this->assertArrayHasKey('from', $response);
-        $this->assertEquals('2001-01-04T00:00:00+00:00', $response['from']);
+        $this->assertEquals('2021-01-04T00:00:00+00:00', $response['from']);
         $this->assertArrayHasKey('to', $response);
-        $this->assertEquals('2001-01-04T00:00:00+00:00', $response['to']);
+        $this->assertEquals('2021-01-04T00:00:00+00:00', $response['to']);
         $this->assertArrayHasKey('avg', $response);
         $this->assertEquals(228.1, $response['avg']);
     }
@@ -29,7 +29,7 @@ class GoldControllerTest extends WebTestCase
     public function testGoldJanuary2021Range()
     {
         $client = static::createClient();
-        $client->xmlHttpRequest('POST', '/api/gold', [
+        $client->jsonRequest('POST', '/api/gold', [
             'from' => '2021-01-01T00:00:00+00:00',
             'to' => '2021-01-31T00:00:00+00:00'
         ]);
@@ -47,7 +47,7 @@ class GoldControllerTest extends WebTestCase
     public function testMissingTimezone()
     {
         $client = static::createClient();
-        $client->xmlHttpRequest('POST', '/api/gold', [
+        $client->jsonRequest('POST', '/api/gold', [
             'from' => '2001-01-04 00:00:00',
             'to' => '2001-01-04 00:00:00'
         ]);
