cb376f63d4f657542bf812bd9e8c83b2f208699a
Szymon Studniarz <szymek1019@gmail.com>
2022-07-14 15:40:33 +0200

    add ISO8601 date format and JSON validation isValidISO8601Date method uses regex to check if date is in valid format

--- src/Controller/GoldController.php
+++ src/Controller/GoldController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 date_default_timezone_set("Europe/Warsaw");
 
@@ -13,14 +14,17 @@ class GoldController extends AbstractController
 {
     #[Route("/api/gold", name: "app_gold", methods: ["POST"])]
     public function index(Request $request): JsonResponse {
+        if(!GoldController::isJSONValid($request->getContent())){
+            return $this->json([])->setStatusCode('400');
+        }
         $timestamps = json_decode($request->getContent(), true);
         $startDate = date("Y-m-d", strtotime($timestamps["from"]));
         $endDate = date("Y-m-d", strtotime($timestamps["to"]));
         $url = "http://api.nbp.pl/api/cenyzlota/{$startDate}/{$endDate}";
         $goldPrices = json_decode(file_get_contents($url), true);
-        $sumOfGoldPrices=0;
-        foreach($goldPrices as $value){
-            $sumOfGoldPrices+=$value["cena"];
+        $sumOfGoldPrices = 0;
+        foreach ($goldPrices as $value) {
+            $sumOfGoldPrices += $value["cena"];
         }
         return $this->json([
             "from" => date_format(
@@ -31,7 +35,30 @@ class GoldController extends AbstractController
                 date_create($goldPrices[count($goldPrices) - 1]["data"]),
                 DATE_W3C
             ),
-            "avg" => round($sumOfGoldPrices/count($goldPrices),2),
+            "avg" => round($sumOfGoldPrices / count($goldPrices), 2),
         ]);
+    }
+    public static function isValidISO8601Date(string $date): bool
+    {
+        if (
+            preg_match(
+                "/\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?/i",
+                $date
+            )
+        ) {
+            return true;
+        }
+        return false;
+    }
+    public static function isJSONValid(string $json): bool
+    {
+        try {
+            if(is_null(json_decode($json, null))){
+                throw new Exception("Invalid JSON", 1);
+            }
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
     }
 }
